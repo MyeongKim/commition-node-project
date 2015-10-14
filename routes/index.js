d88bf28df4f76576ad8db6de2ac1e0df4f041853
mingkim <ming3772@gmail.com>
2015-10-14 21:08:11 +0900

    ADD - search by aglin with autoScroll

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,77 @@
 var express = require('express');
 var router = express.Router();
 
+var Commition = require('../models/commition');
+
+String.prototype.toObjectId = function() {
+  var ObjectId = (require('mongoose').Types.ObjectId);
+  return new ObjectId(this.toString());
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {user : req.user});
+  res.redirect('/search/view');
 });
 
-router.get('/scroll', function(req, res, next) {
-  res.render('scrollContent',{layout : false});
+router.get('/scroll/new/:lastValue', function(req, res, next) {
+	var lastValue = req.params.lastValue;
+	Commition.find({time : {$lt : lastValue}}).sort({'time' : -1}).limit(10).exec(function(err, commitions) {
+	    if(err){
+	      console.log(err);
+	    }
+	    if(Object.keys(commitions).length !== 0){
+	    	var options = {
+				commitions : commitions,
+				align : 'new',
+				lastValue : commitions[commitions.length-1].time,
+				layout : false
+	  		}
+    		res.render('scrollContent', options);
+	    }else{
+	    	res.end();
+	    }
+  	});
 });
 
+router.get('/scroll/view/:lastValue', function(req, res, next) {
+	var lastValue = req.params.lastValue;
+	Commition.find({view : {$lt : lastValue}}).sort({'view' : -1}).limit(10).exec(function(err, commitions) {
+	    if(err){
+	      console.log(err);
+	    }
+	    if(Object.keys(commitions).length !== 0){
+	    	var options = {
+				commitions : commitions,
+				align : 'view',
+				lastValue : commitions[commitions.length-1].view,
+				layout : false
+	  		}
+    		res.render('scrollContent', options);
+	    }else{
+	    	res.end();
+	    }
+  	});
+});
+
+router.get('/scroll/duedate/:lastValue', function(req, res, next) {
+	var lastValue = req.params.lastValue;
+	Commition.find({end_time : {$gt : lastValue}}).sort({'end_time' : 1}).limit(10).exec(function(err, commitions) {
+	    if(err){
+	      console.log(err);
+	    }
+	    if(Object.keys(commitions).length !== 0){
+	    	var options = {
+				commitions : commitions,
+				align : 'duedate',
+				lastValue : commitions[commitions.length-1].end_time,
+				layout : false
+	  		}
+    		res.render('scrollContent', options);
+	    }else{
+	    	res.end();
+	    }
+  	});
+});
 // commition page
 router.get('/detail', function(req, res, next) {
   res.render('detail', {logInUser : req.user});
--- routes/search.js
+++ routes/search.js
@@ -4,23 +4,48 @@ var router = express.Router();
 var Commition = require('../models/commition');
 
 router.get('/new', function(req, res, next) {
-  Commition.find({}, function(err, commitions){
-  	if(err){
-  		console.log(err);
-  	}
-  	var model = {
-  		commitions : commitions
-  	}
-  	res.render('index', model);
+  Commition.find({}).sort({'time' : -1}).limit(10).exec(function(err, commitions) {
+    if(err){
+      console.log(err);
+    }
+    var options = {
+      commitions : commitions,
+      align : 'new',
+      lastValue : commitions[commitions.length-1].time,
+      moreLoad : (commitions.length > 10 ? true : false)
+    }
+    res.render('index', options);
   });
 });
 
 router.get('/view', function(req, res, next) {
-  res.render('index');
+  Commition.find({}).sort({'view' : -1}).limit(10).exec(function(err, commitions) {
+    if(err){
+      console.log(err);
+    }
+    var options = {
+      commitions : commitions,
+      align : 'view',
+      lastValue : commitions[commitions.length-1].view,
+      moreLoad : (commitions.length > 10 ? true : false)
+    }
+    res.render('index', options);
+  });
 });
 
 router.get('/duedate', function(req, res, next) {
-  res.render('index');
+  Commition.find({}).sort({'end_time' : 1 }).limit(10).exec(function(err, commitions) {
+    if(err){
+      console.log(err);
+    }
+    var options = {
+      commitions : commitions,
+      align : 'duedate',
+      lastValue : commitions[commitions.length-1].end_time,
+      moreLoad : (commitions.length > 10 ? true : false)
+    }
+    res.render('index', options);
+  });
 });
 
 module.exports = router;
